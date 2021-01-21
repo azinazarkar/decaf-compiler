9b0c15c6afbfc9be5d999f030a449d560465fff4
Roozbeh <roozbehsayadi@gmail.com>
2021-01-21 21:05:32 +0330

    Added a dummy CodeGen for now.

---
+++ src/compiler/CodeGenerator/CodeGen.java
@@ -0,0 +1,21 @@
+package compiler.CodeGenerator;
+
+public class CodeGen {
+	private static CodeGen ourInstance = new CodeGen();
+
+	public static CodeGen getInstance() {
+		return ourInstance;
+	}
+
+	private CodeGen() {
+	}
+
+	public void compile() {
+
+	}
+
+	public void cgen() {
+
+	}
+
+}
