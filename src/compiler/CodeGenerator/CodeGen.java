b7954c81c837290d9f1e1caeba6a9efc48f8ba85
Roozbeh <roozbehsayadi@gmail.com>
2021-01-22 13:03:22 +0330

    Added data segment and text segment to code generator + compile function which writes data segment and text segment into a file.

--- src/compiler/CodeGenerator/CodeGen.java
+++ src/compiler/CodeGenerator/CodeGen.java
@@ -1,7 +1,12 @@
 package compiler.CodeGenerator;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class CodeGen {
 	private static CodeGen ourInstance = new CodeGen();
+	private String dataSeg = "\t.data\n";
+	private String textSeg = "\t.text\n.global main\nmain:\n";
 
 	public static CodeGen getInstance() {
 		return ourInstance;
@@ -10,12 +15,28 @@ public class CodeGen {
 	private CodeGen() {
 	}
 
-	public void compile() {
+	public void addToData( String in ) {
+		dataSeg = dataSeg + "\t" + in + "\n";
+	}
 
+	public void addToData( String name, String type, String value ) {
+		addToData( name + ": " + type + " " + value );
+	}
+
+	public void addToText( String in, boolean isLabel ) {
+		if ( !isLabel )
+			textSeg = textSeg + "\t";
+		textSeg = textSeg + in + "\n";
 	}
 
 	public void cgen() {
 
+	}
+
+	public void compile() throws IOException {
+		FileWriter out = new FileWriter( "out.asm" );
+		out.write( dataSeg + textSeg);
+		out.close();
 	}
 
 }
