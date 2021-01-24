6da801cbdeae959d6bd3e40bdd835d3b73851d3c
Roozbeh <roozbehsayadi@gmail.com>
2021-01-25 00:11:50 +0330

    Added sum for two doubles.

--- out.asm
+++ out.asm
@@ -6,22 +6,18 @@
 	aa: .word 0
 	ab: .word 0
 	ac: .word 0
-	ad: .word 0
-	ae: .word 0
-	af: .word 0
-	_ag: .word 79
-	_ah: .word 5
-	_ai: .word -5
-	_aj: .word 79
-	_ak: .word -395
+	_ad: .word 79
+	_ae: .word 1078523331
+	_af: .word 1078523331
+	_ag: .word 1086911939
 
 	.text
 	.globl main
 
 test:
 
-	# Assigning _ag to ab
-	lw $a0, _ag
+	# Assigning _ad to ab
+	lw $a0, _ad
 	la $a1, ab
 	sw $a0, 0($a1)
 	
@@ -37,63 +33,17 @@ test:
 	
 main:
 
-	# Getting negative value of _ah
-	lw $s0, _ah
-	neg $s1, $s0
-	sw $s1, _ai
+	# Adding _ae and _af
+	lwc1 $f0, _ae
+	lwc1 $f1, _af
+	add.s $f2, $f0, $f1
+	la $a0, _ag
+	swc1 $f2, 0($a0)
 	
-	# Assigning _ai to ac
-	lw $a0, _ai
-	la $a1, ac
-	sw $a0, 0($a1)
-	
-	# Assigning _aj to ad
-	lw $a0, _aj
-	la $a1, ad
-	sw $a0, 0($a1)
-	
-	# Multiplying ac by ad
-	lw $a0, ac
-	lw $a1, ad
-	mult $a0, $a1
-	la $a2, _ak
-	mflo $t0
-	sw $t0, 0($a2)
-	
-	# Assigning _ak to ae
-	lw $a0, _ak
-	la $a1, ae
-	sw $a0, 0($a1)
-	
-	# Printing ac
-	li $v0, 1
-	lw $a0, ac
-	syscall
-	
-	# Printing new line
-	li $a0, 0xA
-	li $v0, 0xB
-	syscall
-	
-	# Printing ad
-	li $v0, 1
-	lw $a0, ad
-	syscall
-	
-	# Printing new line
-	li $a0, 0xA
-	li $v0, 0xB
-	syscall
-	
-	# Printing ae
-	li $v0, 1
-	lw $a0, ae
-	syscall
-	
-	# Printing new line
-	li $a0, 0xA
-	li $v0, 0xB
-	syscall
+	# Assigning _ag to ac
+	lwc1 $f0, _ag
+	la $a0, ac
+	swc1 $f0, 0($a0)
 	
 	# Exit!
 	li $v0, 10
