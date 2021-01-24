5860ad73f441965cc65796b6dfa1f1e0737eb43b
Azin Azarkar <azin.azarkar@gmail.com>
2021-01-24 21:47:57 +0330

    less added

--- src/compiler/CodeGenerator/CodeGen/LessCodeGen.java
+++ src/compiler/CodeGenerator/CodeGen/LessCodeGen.java
@@ -1,7 +1,11 @@
 package compiler.CodeGenerator.CodeGen;
 
+import compiler.CodeGenerator.Exceptions.CalculationTypeMismatch;
+import compiler.CodeGenerator.IDGenerator;
 import compiler.CodeGenerator.SemanticStack;
+import compiler.CodeGenerator.SymbolTable.SymbolTable;
 import compiler.CodeGenerator.SymbolTable.Utility.Descriptor;
+import compiler.CodeGenerator.SymbolTable.Utility.Type;
 
 public class LessCodeGen {
     private static LessCodeGen ourInstance = new LessCodeGen();
@@ -9,6 +13,25 @@ public class LessCodeGen {
     private LessCodeGen(){}
     public void cgen(){
         // azin is working
-        Descriptor d2 = SemanticStack.getInstance().popDescriptor();
+        Descriptor d2 = (Descriptor) SemanticStack.getInstance().popDescriptor();
+        Descriptor d1 = (Descriptor) SemanticStack.getInstance().popDescriptor();
+        CodeGen.getInstance().addToText("# Is " + d1.getName() + "less than " + d2.getName() + "? ");
+        if (d1.getType() != d2.getType())
+            throw new CalculationTypeMismatch("<", d1.getType(), d2.getType());
+        if (d1.getType() == Type.INT){
+            Descriptor operationResult = new Descriptor(
+                    "_" + IDGenerator.getInstance().getNextID(),
+                    Type.BOOL,
+                    (int)d1.getValue() < (int)d2.getValue()
+            );
+            SymbolTable.getInstance().getSymbolTable().addEntry(operationResult.getName(), operationResult);
+            CodeGen.getInstance().addToText("lw " + "$a0, " + d1.getName());
+            CodeGen.getInstance().addToText("lw " + "$a1, " + d2.getName());
+            CodeGen.getInstance().addToText("slt $t0, $a0, $a1");
+            CodeGen.getInstance().addToText("la " + "$a2, " + operationResult.getName());
+            CodeGen.getInstance().addToText("sw $t0, 0($a2)");
+            CodeGen.getInstance().addEmptyLine();
+            SemanticStack.getInstance().pushDescriptor( operationResult );
+        }
     }
 }
